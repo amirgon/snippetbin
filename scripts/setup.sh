5b694cb75bbe7eef5b1324bf305e6247f672fc10
Amir Gonnen <amirgonnen@gmail.com>
2019-08-20 00:03:36 +0300

    script bugfixes for heroku

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Set up SSH key and git credentials
-# Expects SNIPPETBIN_DEPLOY_KEY and optionally SNIPPETBIN_DATA_DIR, SNIPPETBIN_REMOTE_DATA to be defined at this point
+# Expects SNIPPETBIN_DEPLOY_KEY and optionally SNIPPETBIN_DATA_DIR, SNIPPETBIN_REMOTE_HOST, SNIPPETBIN_REMOTE_REPO to be defined at this point
 # Use: heroku config:set SNIPPETBIN_DEPLOY_KEY="$(cat ~/.ssh/snippetbin_service)"
 
-export data_dir=${SNIPPETBIN_DATA_DIR:-~/online_data}
-export remote_data=${SNIPPETBIN_REMOTE_DATA:-git@github.com:amirgon/snippetbin_data.git}
+export data_dir=${SNIPPETBIN_DATA_DIR:-/app/data}
+export remote_host=${SNIPPETBIN_REMOTE_HOST:-github.com}
+export remote_repo=${SNIPPETBIN_REMOTE_REPO:-amirgon/snippetbin_data.git}
 
 mkdir -p $data_dir
 [ -e "$data_dir/.git" ] && exit 0
@@ -17,7 +18,7 @@ echo "$SNIPPETBIN_DEPLOY_KEY" > ~/.ssh/snippetbin_service
 cat << EOF > ~/.ssh/config
 
 Host snippetbin_service
-    HostName github.com
+    HostName $remote_host
     User git
     IdentityFile ~/.ssh/snippetbin_service
     IdentitiesOnly yes
@@ -25,7 +26,7 @@ Host snippetbin_service
 EOF
 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-git clone ssh://git@snippetbin_service/amirgon/snippetbin_data.git $data_dir
+git clone ssh://git@snippetbin_service/$remote_repo $data_dir
 git config -f $data_dir/.git/config user.email "service@snippet-bin.herokuapp.com"
 git config -f $data_dir/.git/config user.name "service"
 
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-yes | git pull --all
-yes | git push --all
+git pull --all
+git push --all
 
